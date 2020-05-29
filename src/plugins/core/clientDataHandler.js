75e1fc59da782f024be636514ccc24aa58ab7d4a
ricky <sil3nt884@hotmail.co.uk>
2020-05-30 00:22:07 +0100

    allow cors

--- src/plugins/core/clientDataHandler.js
+++ src/plugins/core/clientDataHandler.js
@@ -5,7 +5,8 @@ module.exports = (broker) => {
         const headers = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-            "Access-Control-Max-Age": 2592000 // 30 days
+            "Access-Control-Max-Age": 2592000,
+            "Access-Control-Allow-Headers": "*"// 30 days
             /** add other headers as per requirement */
         };
         response.writeHead(200, headers);
