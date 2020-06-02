52a11b5773052f54ace4a8584046568cb516620e
ricky <sil3nt884@hotmail.co.uk>
2020-06-02 19:59:17 +0100

    remove headers used for express

--- src/plugins/core/clientDataHandler.js
+++ src/plugins/core/clientDataHandler.js
@@ -1,15 +1,8 @@
 module.exports = (broker) => {
   broker.on('client data', ({ body, response }) => {
     console.log('body', body)
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-      'Access-Control-Max-Age': 2592000,
-      'Access-Control-Allow-Headers': '*'// 30 days
-      /** add other headers as per requirement */
-    }
-    broker.emit('player object', body);
-    response.writeHead(200, headers)
+    broker.emit('player object', body)
+    response.sendStatus(200)
     response.end()
   })
 }
