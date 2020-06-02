55c1a5b2b5e009484c2408856d3e13a019ef3421
ricky <sil3nt884@hotmail.co.uk>
2020-06-02 20:00:17 +0100

    Remove headers used for express

--- src/plugins/core/endPointHandler.js
+++ src/plugins/core/endPointHandler.js
@@ -25,46 +25,29 @@ module.exports = (broker, config) => {
   })
 
   broker.on('/players', ({ request, response }) => {
-    if (Object.keys(clients).length > 1) {
-      const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000,
-        'Access-Control-Allow-Headers': '*'// 30 days
-        /** add other headers as per requirement */
-      }
-      response.writeHead(200, headers)
+    console.log(Object.keys(clients).length)
+    if (Object.keys(clients).length >= 2) {
+      response.sendStatus(200)
       response.end()
     }
   })
 
   broker.on('/get-data', ({ request, response }) => {
     // If a GET request is received
-    const address = request.connection.remoteAddress;
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-      'Access-Control-Max-Age': 2592000,
-      'Access-Control-Allow-Headers': '*',// 30 days
-      'Content-Type': 'application/json'
-      /** add other headers as per requirement */
-    }
+    const address = request.connection.remoteAddress
     // Take a player id
-    const playerId = new URL(`https://${request.headers.host}${request.url}`).searchParams;
+    const playerId = new URL(`https://${request.headers.host}${request.url}`).searchParams
 
     // Only if two players connected
     if (Object.keys(clients).length > 1) {
-
       // Send player 2 object
       broker.on('player object', (body) => {
-        console.log(body);
-        response.writeHead(200, headers);
-        response.write(body);
-        response.end();
-      });
+        response.send(body)
+        response.end()
+      })
     } else {
-      response.writeHead(404, headers);
-      response.end();
+      response.sendStatus(404)
+      response.end()
     }
-  });
+  })
 }
