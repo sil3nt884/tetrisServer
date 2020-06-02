7d5be78a8cb456981b0580c6a9cf0b1683160279
ricky <sil3nt884@hotmail.co.uk>
2020-06-02 20:02:13 +0100

    Generate unique ID for user

--- src/plugins/core/newClientsHandler.js
+++ src/plugins/core/newClientsHandler.js
@@ -1,13 +1,16 @@
+const crypto = require('crypto')
 module.exports = (broker) => {
   const clientsObj = {}
-  let ids = 0
+  let lastClientObj
   broker.on('new client added', ({ request, response }) => {
+    const generatePlayerId = () => crypto.createHash('sha256').update(new Date().toISOString() + Object.keys(clientsObj).length).digest('hex')
     if (!clientsObj[request.connection.remoteAddress]) {
       clientsObj[request.connection.remoteAddress] = {
-        clientId: ids
+        clientId: generatePlayerId()
       }
+      lastClientObj = clientsObj[request.connection.remoteAddress]
+      broker.emit('last client to join', lastClientObj)
     }
-    ids++
   })
   broker.on('updated client list', () => broker.emit('updated client Object', clientsObj))
 }
