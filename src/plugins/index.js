ca49998249f01338ef92b01e78a74fc505e84385
ricky <sil3nt884@hotmail.co.uk>
2020-06-02 20:01:19 +0100

    Add Express Server

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,14 +1,15 @@
 const client = require('./core/clientsReciever')
 const newClientHandler = require('./core/newClientsHandler')
 const clientDataHandler = require('./core/clientDataHandler')
-const serverSecure = require('./core/serverSecure')
+// const serverSecure = require('./core/serverSecure')
 const endpointHandler = require('./core/endPointHandler')
+const expressServer = require('./core/expressServer')
 
 const plugins = {
   client,
   newClientHandler,
   clientDataHandler,
-  serverSecure,
+  expressServer,
   endpointHandler
 }
 
