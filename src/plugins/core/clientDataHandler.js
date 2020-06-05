e952c122fe41ce4a7f223326ada27f97d7c21049
ricky <sil3nt884@hotmail.co.uk>
2020-06-05 21:14:04 +0100

    Fix data endPointHandler.js bug

--- src/plugins/core/clientDataHandler.js
+++ src/plugins/core/clientDataHandler.js
@@ -2,7 +2,6 @@ module.exports = (broker) => {
   broker.on('client data', ({ body, response }) => {
     console.log('body', body)
     broker.emit('player object', body)
-    response.sendStatus(200)
     response.end()
   })
 }
